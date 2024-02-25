3ca15faf7f580990658c658db39c1958c65d1a05
qthawq <nguyenqthangwork@gmail.com>
2024-02-25 08:09:53 +0700

    qthawq-commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,19 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, {message: 'Too short'}),
+    username: z.string().min(2, {message: 'Too short'}),
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Password must be at least 8 charaters'})
+  });
+  export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Password must be at least 8 charaters'})
+  });
+
+  export const PostValidation = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string(),
+  });
